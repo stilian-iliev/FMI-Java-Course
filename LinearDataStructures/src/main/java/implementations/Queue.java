7931864cfdc43eeb51ce1b824bcc30575756aff8
Stilian Iliev <74832022+stilian-iliev@users.noreply.github.com>
2022-10-17 04:11:09 +0300

    queue implementation

--- LinearDataStructures/src/main/java/implementations/Queue.java
+++ LinearDataStructures/src/main/java/implementations/Queue.java
@@ -5,33 +5,71 @@ import interfaces.AbstractQueue;
 import java.util.Iterator;
 
 public class Queue<E> implements AbstractQueue<E> {
+    private Node<E> bottom;
+    private int size;
+    private static class Node<E> {
+        private E value;
+        private Node<E> next;
+
+        public Node(E value) {
+            this.value = value;
+        }
+    }
     @Override
     public void offer(E element) {
+        if (bottom == null) {
+            bottom = new Node<>(element);
+        } else {
+            Node<E> current = bottom;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = new Node<>(element);
+        }
+        size++;
 
     }
 
     @Override
     public E poll() {
-        return null;
+        if (isEmpty()) throw new IllegalStateException();
+        E tmp = bottom.value;
+        bottom = bottom.next;
+        size--;
+        return tmp;
     }
 
     @Override
     public E peek() {
-        return null;
+        if (isEmpty()) throw new IllegalStateException();
+        return bottom.value;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            private Node<E> current = bottom;
+            @Override
+            public boolean hasNext() {
+                return current.next != null;
+            }
+
+            @Override
+            public E next() {
+                E tmp = current.value;
+                current = current.next;
+                return tmp;
+            }
+        };
     }
 }
