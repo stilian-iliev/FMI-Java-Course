8399de5b3d524c0ad427693269effefbea916a1c
Stilian Iliev <74832022+stilian-iliev@users.noreply.github.com>
2022-10-17 08:33:37 +0300

    singly linked list

--- LinearDataStructures/src/main/java/implementations/SinglyLinkedList.java
+++ LinearDataStructures/src/main/java/implementations/SinglyLinkedList.java
@@ -5,49 +5,104 @@ import interfaces.LinkedList;
 import java.util.Iterator;
 
 public class SinglyLinkedList<E> implements LinkedList<E> {
+    private Node<E> head;
+    private int size;
+
+    private static class Node<E> {
+        private E value;
+        private Node<E> next;
+
+        public Node(E value) {
+            this.value = value;
+        }
+    }
+
+    public SinglyLinkedList() {
+        size = 0;
+    }
 
     @Override
     public void addFirst(E element) {
+        Node<E> newNode = new Node<>(element);
 
+        if (!isEmpty()) newNode.next = head;
+        head = newNode;
+        size++;
     }
 
     @Override
     public void addLast(E element) {
+        if (isEmpty()) {
+            head = new Node<>(element);
+        } else {
+            Node<E> current = head;
 
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = new Node<>(element);
+        }
+        size++;
     }
 
     @Override
     public E removeFirst() {
-        return null;
+        E tmp = head.value;
+        head = head.next;
+        size--;
+        return tmp;
     }
 
     @Override
     public E removeLast() {
-        return null;
+        Node<E> preCurrent = head;
+        Node<E> current = head.next;
+        while (current.next != null) {
+            preCurrent = current;
+            current = current.next;
+        }
+        E tmp = current.value;
+        preCurrent.next = null;
+        size--;
+        return tmp;
     }
 
     @Override
     public E getFirst() {
-        return null;
+        return head.value;
     }
 
     @Override
     public E getLast() {
-        return null;
+        Node<E> current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        return current.value;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public E next() {
+                return null;
+            }
+        };
     }
 }
