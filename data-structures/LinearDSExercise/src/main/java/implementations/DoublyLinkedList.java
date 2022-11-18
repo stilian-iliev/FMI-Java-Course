0289989e92666cae1aa6bfe671956c61e83fe2db
Stilian Iliev <74832022+stilian-iliev@users.noreply.github.com>
2022-11-18 04:12:09 +0200

    faster queue and faster doublylinkedlist

---
+++ data-structures/LinearDSExercise/src/main/java/implementations/DoublyLinkedList.java
@@ -0,0 +1,128 @@
+package implementations;
+
+import interfaces.LinkedList;
+
+import java.util.Iterator;
+
+public class DoublyLinkedList<E> implements LinkedList<E> {
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
+
+    private static class Node<E> {
+        private E element;
+        private Node<E> previous;
+        private Node<E> next;
+
+        public Node(E value) {
+            this.element = value;
+        }
+    }
+
+    public DoublyLinkedList() {
+    }
+
+    @Override
+    public void addFirst(E element) {
+        Node<E> newNode = new Node<>(element);
+        if (this.head != null) {
+            newNode.next = this.head;
+            this.head.previous = newNode;
+            this.head = newNode;
+        } else {
+            this.tail = this.head = newNode;
+        }
+
+        this.size++;
+    }
+
+    @Override
+    public void addLast(E element) {
+        Node<E> newNode = new Node<>(element);
+        if (this.head != null) {
+            newNode.previous = this.tail;
+            this.tail.next = newNode;
+            this.tail = this.tail.next;
+        } else {
+            this.head = this.tail = newNode;
+        }
+        this.size++;
+    }
+
+    @Override
+    public E removeFirst() {
+        ensureNotEmpty();
+        E element = this.head.element;
+        if (this.size == 1) {
+            this.head = this.tail = null;
+        } else {
+            this.head = this.head.next;
+            this.head.previous = null;
+        }
+        this.size--;
+        return element;
+    }
+
+    private void ensureNotEmpty() {
+        if (this.size == 0) {
+            throw new IllegalStateException("Illegal remove for empty LinkedList");
+        }
+    }
+
+    @Override
+    public E removeLast() {
+        ensureNotEmpty();
+        if (this.size == 1) {
+            return removeFirst();
+        }
+        E element =  tail.element;
+
+        tail = tail.previous;
+        tail.next = null;
+        this.size--;
+
+        return element;
+    }
+
+    @Override
+    public E getFirst() {
+        ensureNotEmpty();
+        return this.head.element;
+    }
+
+    @Override
+    public E getLast() {
+        ensureNotEmpty();
+        return tail.element;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private Node<E> current = head;
+
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public E next() {
+                E element = current.element;
+                current = current.next;
+                return element;
+            }
+        };
+    }
+}
