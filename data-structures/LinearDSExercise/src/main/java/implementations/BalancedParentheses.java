55201a91d478358f6c4a83e28b35cfb69dcacf5b
Stilian Iliev <74832022+stilian-iliev@users.noreply.github.com>
2022-11-18 11:53:09 +0200

    balanced parentheses

--- data-structures/LinearDSExercise/src/main/java/implementations/BalancedParentheses.java
+++ data-structures/LinearDSExercise/src/main/java/implementations/BalancedParentheses.java
@@ -2,8 +2,12 @@ package implementations;
 
 import interfaces.Solvable;
 
+import java.util.Stack;
+
 public class BalancedParentheses implements Solvable {
     private String parentheses;
+    private String openingBrackets = "[{(";
+    private String closingBrackets = "]})";
 
     public BalancedParentheses(String parentheses) {
         this.parentheses = parentheses;
@@ -11,6 +15,22 @@ public class BalancedParentheses implements Solvable {
 
     @Override
     public Boolean solve() {
-        return null;
+        Stack<Character> bracketsStack = new Stack<>();
+        for (char c : parentheses.toCharArray()) {
+            char last = bracketsStack.isEmpty() ? '-' : bracketsStack.peek();
+            if (openingBrackets.contains(String.valueOf(c))) {
+                bracketsStack.push(c);
+            } else if (last == '(' && c == ')') {
+                bracketsStack.pop();
+            } else if (last == '[' && c == ']') {
+                bracketsStack.pop();
+            } else if (last == '{' && c == '}') {
+                bracketsStack.pop();
+            } else {
+                return false;
+            }
+        }
+        return bracketsStack.isEmpty();
     }
+
 }
